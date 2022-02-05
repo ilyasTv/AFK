799d45864edac26bd11d9bcf18fe213477f77bcd
Saino20 <98251558+Saino20@users.noreply.github.com>
2022-02-05 20:56:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let data = JSON.parse(rawdata);
 var lasttime = -1;
 var moving = 0;
 var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
+var actions = [ 'forward', 'forward', 'forward', 'back', 'back', 'back', 'left', 'left', 'left', 'right', 'right', 'right']
 var lastaction;
 var pi = 3.14159;
-var moveinterval = 2; // 2 second movement interval
-var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
+var moveinterval = 1; // 2 second movement interval
+var maxrandom = 2; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
 var username = data["name"]
 var nightskip = data["auto-night-skip"]
